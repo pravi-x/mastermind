864a07f03150f9ec167469c9303d7079c63319ee
pravi-x <1998.xar@gmail.com>
2024-09-12 16:46:04 +0300

    fix color matches counter, game over output

--- lib/display.rb
+++ lib/display.rb
@@ -15,17 +15,16 @@ module Display
   def display_input_warning
     ">> I didn't understand your guess. Try again <<"
   end
-
   def display_secret_code(code)
-    ">>> SECRET CODE | #{code[0]} | #{code[1]} | #{code[2]} | #{code[3]} | <<<"
+    ">>> SECRET CODE | #{code[0].to_s} | #{code[1].to_s} | #{code[2].to_s} | #{code[3].to_s} | <<<"
   end
 
   def display_wining_message
     "\n\n>> Congratulations! You've guessed the secret code! <<"
   end
 
-  def display_game_over(code)
-    "\n\n>> Game over! You've reached the maximum number of moves. <<\n" << display_secret_code(code)
+  def display_game_over
+    "\n\n>> Game over! You've reached the maximum number of moves. <<\n" 
   end
 
   def display_move_number(number)
--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+
 
 require_relative 'display'
 
@@ -9,7 +9,7 @@ class Game
     puts display_intro
     @secret_code = generate_secret_code
     @move = 0
-    @max_moves = 12
+    @max_moves = 2
   end
   COLORS = %i[red blue green yellow orange purple].freeze
 
@@ -29,7 +29,8 @@ class Game
       puts display_winning_message
       true
     elsif @move >= @max_moves
-      puts display_game_over(@secret_code)
+      puts display_game_over
+      puts display_secret_code(@secret_code)
       true
     else
       false
@@ -43,17 +44,18 @@ class Game
     # Check for exact matches
     exact_matches = guess_copy.each_with_index.count { |color, index| color == secret_code_copy[index] }
     # Check for color matches
-    color_matches = guess_copy.count { |color| color && secret_code_copy.include?(color) }
+    color_matches = guess_copy.each_with_index.count { |color, index| color && secret_code_copy.include?(color) && !(color == secret_code_copy[index]) }
 
     puts display_feedback(exact_matches, color_matches)
   end
 
   def generate_secret_code
-    c1 = COLORS.sample(1)
-    c2 = COLORS.sample(1)
-    c3 = COLORS.sample(1)
-    c4 = COLORS.sample(1)    
-    [c1,c2,c3,c4]
+    #c1 = COLORS.sample(1)
+   # c2 = COLORS.sample(1)
+    #c3 = COLORS.sample(1)
+   # c4 = COLORS.sample(1)    
+    #[c1,c2,c3,c4]
+    %i[red blue red red]
   end
 
   def guess_from_player
